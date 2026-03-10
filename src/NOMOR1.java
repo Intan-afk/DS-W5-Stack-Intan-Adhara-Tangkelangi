a73f8b93d25342bb5dc6cf9b88aafe9ddc4a3bc2
Intan Adhara TAngkelangi <itangkelangi@student.ciputra.ac.id>
2026-03-10 09:00:37 +0800

    update ulang NOMOR 1

---
+++ src/NOMOR1.java
@@ -0,0 +1,39 @@
+import java.util.Stack;
+public class NOMOR1 {
+ public static void main(String[] args) {
+        String test = "({[]})";
+        if (isBalanced(test)) {
+            System.out.println("Balanced.");
+        } else {
+            System.out.println("Not Balanced.");
+        }
+        
+    }
+     public static boolean isBalanced(String input) {
+        Stack<Character> stack = new Stack<>();
+        
+        for (char c : input.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else if (c == ')' || c == '}' || c == ']') {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                char top = stack.pop();
+                if ((c == ')' && top != '(') ||
+                    (c == '}' && top != '{') ||
+                    (c == ']' && top != '[')) {
+                    return false;
+                    
+                }
+            }
+        }
+        
+        if (stack.isEmpty()) {
+            System.out.println("Valid expression");
+        } else {
+            System.out.println("Invalid expression");
+        }
+        return stack.isEmpty();
+    }
+}
